408f3b7d53f4a27fd7a33c36d5c69ae8ed7dd061
alainabuzas <alaina.buzas@gmail.com>
2017-05-16 16:40:10 -0700

    beginning to scrape down boilerplate

---
+++ public/app/controllers.js
@@ -0,0 +1,87 @@
+angular.module('RecipeCtrls', ['RecipeServices'])
+    .controller('HomeCtrl', ['$scope', 'Recipe', function($scope, Recipe) {
+        $scope.recipes = [];
+
+        Recipe.query(function success(data) {
+            $scope.recipes = data;
+        }, function error(data) {
+            console.log(data);
+        });
+
+        $scope.deleteRecipe = function(id, recipesIdx) {
+            Recipe.delete({ id: id }, function success(data) {
+                $scope.recipes.splice(recipesIdx, 1);
+            }, function error(data) {
+                console.log(data);
+            });
+        };
+    }])
+    .controller('ShowCtrl', ['$scope', '$stateParams', 'Recipe', function($scope, $stateParams, Recipe) {
+        $scope.recipe = {};
+
+        Recipe.get({ id: $stateParams.id }, function success(data) {
+            $scope.recipe = data;
+        }, function error(data) {
+            console.log(data);
+        });
+    }])
+    .controller('NewCtrl', ['$scope', '$location', 'Recipe', function($scope, $location, Recipe) {
+        $scope.recipe = {
+            title: '',
+            description: '',
+            image: ''
+        };
+
+        $scope.createRecipe = function() {
+            Recipe.save($scope.recipe, function success(data) {
+                $location.path('/');
+            }, function error(data) {
+                console.log(data);
+            });
+        };
+    }])
+    .controller('NavCtrl', ['$scope', 'Auth', '$state', 'Alerts', function($scope, Auth, $state, Alerts) {
+        $scope.Auth = Auth;
+        $scope.logout = function() {
+            Auth.removeToken();
+            Alerts.add('success', 'You logged out');
+            $state.reload();
+            console.log('My Token:', Auth.getToken());
+        }
+    }])
+    .controller('SignupCtrl', ['$scope', '$http', '$location', 'Alerts', function($scope, $http, $location, Alerts) {
+        $scope.user = {
+            email: '',
+            password: ''
+        };
+        $scope.userSignup = function() {
+            $http.post('/api/users', $scope.user).then(function success(res) {
+                    Alerts.add('success', 'You signed up');
+                    $location.path('/');
+                },
+                function error(res) {
+                    Alerts.add('danger', 'Error. Please see console');
+                    console.log(res);
+                });
+        }
+    }])
+    .controller('LoginCtrl', ['$scope', '$http', '$location', 'Auth', 'Alerts', function($scope, $http, $location, Auth, Alerts) {
+        $scope.user = {
+            email: '',
+            password: ''
+        };
+        $scope.userLogin = function() {
+            $http.post('/api/auth', $scope.user).then(function success(res) {
+                Auth.saveToken(res.data.token);
+                Alerts.add('success', 'You logged in');
+                console.log('Token is', res.data.token);
+                $location.path('/');
+            }, function error(res) {
+                Alerts.add('danger', 'Incorrect email/password');
+                console.log(res);
+            })
+        }
+    }])
+    .controller('AlertCtrl', ['$scope', 'Alerts', function($scope, Alerts) {
+        $scope.Alerts = Alerts;
+    }]);
